37ca461303fbc5edf108221ede242b1d05e0f93b
Lisa <mayerlbp@hotmail.com>
2019-12-15 12:57:54 -0500

    Add permission for jenkins

---
+++ 02-install-docker.sh
@@ -0,0 +1,9 @@
+
+sudo yum check-update
+curl -fsSL https://get.docker.com/ | sh
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl status docker
+
+sudo usermod -aG docker $(whoami)
+sudo usermod -aG docker jenkins
